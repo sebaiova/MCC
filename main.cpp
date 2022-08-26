a5589a5ca614ff022dec6b971d4e13e71ca8c9be
Iovaldi, Sebastian <sebaiova95@gmail.com>
2022-08-25 23:44:14 -0300

    -

--- main.cpp
+++ main.cpp
@@ -7,16 +7,22 @@
 
 double f(double x)
 {
+	return std::tan(x);
+}
+
+/*double f(double x)
+{
 	return std::pow(x, 3) - 2*x -1;
 }
 
 double dxf(double x)
 {
 	return std::pow(3*x, 2) - 2;
-}
+}*/
 
 int main()
 {
+	std::cout << "Utilizando Simpson...\n";
 	double resultado = simpson(f);
 	std::cout << "Resultado: " << resultado << "\n";
 	return 0;
